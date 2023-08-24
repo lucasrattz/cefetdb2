0e674fb814e8ea41798b4d8f5d524c73a700f44d
Lucas Rattz <41240438+lucasrattz@users.noreply.github.com>
2023-08-24 15:19:34 -0300

    Revert "Feature/separate disciplines routes"

--- backend/server/api_resource.go
+++ backend/server/api_resource.go
@@ -21,7 +21,7 @@ func (ar apiResource) Routes() chi.Router {
 
 	if ar.cfg.Server.UseCors {
 		corsHandler := cors.New(cors.Options{
-			AllowedOrigins: []string{ar.cfg.Server.FrontendHost},
+			AllowedOrigins: []string{ar.cfg.Server.Host + ":" + ar.cfg.Server.Port},
 			AllowedMethods: []string{"GET", "POST", "OPTIONS"},
 			AllowedHeaders: []string{"Accept", "Content-Type"},
 			MaxAge:         300,
@@ -36,12 +36,11 @@ func (ar apiResource) Routes() chi.Router {
 	r.Route("/semesters/{semesterID}", func(r chi.Router) {
 		r.Use(ar.semesterMiddleware)
 		r.Get("/disciplines", ar.getDisciplinesBySemester().ServeHTTP)
-	})
-
-	r.Route("/disciplines/{disciplineID}", func(r chi.Router) {
-		r.Use(ar.disciplineMiddleware)
-		r.Get("/files", ar.getFilesByDiscipline().ServeHTTP)
-		r.Post("/files", ar.uploadFile().ServeHTTP)
+		r.Route("/disciplines/{disciplineID}", func(r chi.Router) {
+			r.Use(ar.disciplineMiddleware)
+			r.Get("/files", ar.getFilesByDiscipline().ServeHTTP)
+			r.Post("/files", ar.uploadFile().ServeHTTP)
+		})
 	})
 
 	return r
--- frontend/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./View/App";
-import "./styles/global.scss";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
